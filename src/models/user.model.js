44b0a0b3f6ebfa0b07d5d7aa558b68d6989442a4
Biwash Pradhan <biwashpra3883@gmail.com>
2026-07-21 15:31:31 +0545

    refactor: user model to trim name, email and password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,17 +4,20 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
+      trim: true,
       required: true,
     },
 
     email: {
       type: String,
+      trim: true,
       required: true,
       unique: true,
     },
 
     password: {
       type: String,
+      trim: true,
       required: true,
       select: false, // Mongoose will now automatically strip the password field from all query results by default.
     },
