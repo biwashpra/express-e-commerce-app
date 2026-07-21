9a705c3d49badd36082a421c3dd8171c86134c92
Biwash Pradhan <biwashpra3883@gmail.com>
2026-07-21 11:48:37 +0545

    chore: always use error middleware at last

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// self created middleware
-app.use(errorMiddleware);
-
 // routes configuration
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", checkAuthenticate, userRoute);
+
+// self created middleware
+// Error handler MUST be last
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
